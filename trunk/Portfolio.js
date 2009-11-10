b8c855ad56902b3875974b22f738bd4f019a6fee
markjfe <markjfe@cc660288-c524-11de-8f3f-2f730f80caef>
2009-11-10 07:46:44 +0000

    Finally got the first couple functions in Portfolio.js working.  I need to refer to all methods and properties as this.xxx

--- trunk/Portfolio.js
+++ trunk/Portfolio.js
@@ -2,31 +2,60 @@
 // Definition of the Portfolio class for the GFPortfolio gadget.
 // Each instance of the class is used to store information about one stock.
 
-function Portfolio(symbol)
-// Expect: Nothing
-// Return: Nothing
-// Constructor.  Creates and empty Portfolio object
+function Portfolio()
 {
 	// Create an empty array of stocks
 	this.stocks = new Array();
+	this.addSymbol = addSymbol;
+	this.findSymbol = findSymbol;
+	this.displaySymbols = displaySymbols;
 }
 
-Portfolio.prototype = {
+function findSymbol(sym) {
+// Expect: stock ticker symbol
+// Return: True or False
+// Looks for an element in the 'stocks' array corresponding
+// to the symbol.  Returns true if found and false otherwise
 
-	addSymbol: function(sym) {
-		// Expect: stock ticker symbol
-		// Return: Nothing
-		// Adds the symbol to the Portfolio object
+	debugOutput("findSymbol: stocks: " + this.stocks + " sym: " + sym);
+	for (var i in this.stocks) {
+		debugOutput("findSymbol: i: " + i + "sym: " + this.stocks[i]);
+		if (this.stocks[i] == sym) {
+			return true;
+		}
+	}
+	// If we got through the loop it means we didn't find it
+	return false;
+}
 
-		// Make sure it's not in there first
-		var found = false;
-		for (var i in this.stocks) {
-			if (this.stocks[i] == sym) {
-				found = true;
-			}
-		}
-		if ( !found ) {
-			this.stocks.push(sym);
-		}
-	   }
+function addSymbol (sym) {
+// Expect: stock ticker symbol
+// Return: Nothing
+// Adds the symbol to the Portfolio object
+
+	// Make sure it's not in there first
+	if ( ! this.findSymbol(sym) ) {
+		this.stocks.push(sym);
+		alert("Symbol: " + sym + " added");
+	} else {
+		alert("Symbol: " + sym + " exists");
+	}
+}
+
+function displaySymbols() {
+// Expect: Nothing
+// Return: Nothing
+// Displays the symbols in the stocks array
+	debugOutput("displaySymbols: stocks: " + this.stocks);
+	for (var i in this.stocks) {
+		debugOutput("displaySymbols: Symbol: " + this.stocks[i]);
+	}
+
+}
+
+function debugOutput(text) {
+// Expect: text to print
+// Return: Nothing
+// Prints the text as debug text in the HTML output
+	document.write("Debug: " + text + "<br>\n");
 }
